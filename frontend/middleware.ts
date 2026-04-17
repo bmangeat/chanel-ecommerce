1bcadab1a2f5d62d40553d779d653decc34faf3e
Brice Mangeat <brice.mangeat@gmail.com>
2026-04-17 11:39:52 +0200

    feat: implement JWT-based authentication middleware to protect account, cart, and order routes

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -28,4 +28,22 @@
 //     l'utilisateur est connecté avant d'afficher la page ?"
 // ----------------------------------------------------------------
 
-export {}
+import { NextRequest, NextResponse } from "next/server"
+import { jwtVerify } from 'jose';
+
+export async function middleware(request: NextRequest) {
+  const authToken = request.cookies.get('auth_token')?.value
+
+  if (!authToken) {
+    return NextResponse.redirect(new URL('/login?next=' + request.nextUrl.pathname, request.url))
+  }
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+  try { await jwtVerify(authToken, secret)} catch {
+    return NextResponse.redirect(new URL('/login?next=' + request.nextUrl.pathname, request.url))
+  } 
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/account/:path*', '/cart/:path*', '/orders/:path*'],
+}
